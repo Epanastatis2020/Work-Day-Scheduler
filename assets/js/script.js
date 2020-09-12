2a7475e31b1d54876b320e060ed7359df4ffe803
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-09-12 14:15:25 +1000

    added the moment.js library and current date/time to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
 // for future times, change row (tr) class to "bg-info"
 // for current time, change row (tr) class to "bg-primary"
+
+//Declaring global variables
+
+
+//Functions
+$(document).ready(function() {
+    //init();
+
+var now = moment().format("dddd Do MMMM, YYYY, H:mm");
+$('#currentDateTime').append(now);
+
+});
