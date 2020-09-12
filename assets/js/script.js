965084cf98639a20af4c33579e406969a71e35f4
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-09-12 15:15:52 +1000

    added commentary to javascript file outlining plan for functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,11 @@
 //Declaring global variables
 
 
-//Functions
+
 $(document).ready(function() {
     //init();
 
+// This function sets the current date/time on the jumbatron
 function setCurrentDateTime() {
     var now = moment().format("dddd Do MMMM, YYYY, H:mm");
     $('#currentDateTime').html(now);
@@ -15,5 +16,43 @@ function setCurrentDateTime() {
 };
 
 setCurrentDateTime ();
+
+// This function determines which time slots are accessible for input
+function checkTimeSlot() {
+
+}
+
+// This function is what happens when the "save" button is clicked
+function saveTimeSlot () {
+    //Add code to save input text to local storage referencing time and date
+}
+
+// This function is what happens when the "delete" button is clicked
+function deleteTimeSlot () {
+    //Add code to delete saved input text and date time from local storage
+}
+
+// This function is what happens when the "clear" button is clicked
+function clearTimeSlots () {
+    //Add code to delete all local storage
+}
+
+//This function is what happens when the "previous day" link is clicked
+function previousTimeSlots() {
+    //Add code to update table with previous day's locally stored values
+    //Add code to change jumbatron sub-heading to show previous date instead of current
+}
+
+//This function is what happens when the "current day" link is clicked
+function currentTimeSlots() {
+    //Re-initialise
+}
+
+//This function is what happens when the "next day" link is clicked
+function nextTimeSlots() {
+    //Add code to update table with next day's locally stored values
+    //Add code to change jumbatron sub-heading to show next date instead of current
+}
+
 
 });
